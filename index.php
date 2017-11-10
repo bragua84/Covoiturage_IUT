4f20f530537338d76dff7ebd8bbdef62b4cd333c
Mathias Ughetto <bragua84@gmail.com>
2017-11-10 17:39:49 +0100

    Ajout de la session afin de géré les connexions

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <?php
-  require_once('./lib/File.php');
-  require(File::build_path(array('controller', 'routeur.php')));
+    session_start();
+    require_once('./lib/File.php');
+    require(File::build_path(array('controller', 'routeur.php')));
 
   ?>
